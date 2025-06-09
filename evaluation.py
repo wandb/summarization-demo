2123f3b191ed63ffc22f21fd6e7a3d87360ec4c2
Uma Krishnaswamy <uma.krishnaswamy@wandb.com>
2025-06-09 15:57:21 -0700

    added updated eval

--- evaluation.py
+++ evaluation.py
@@ -7,18 +7,9 @@ import asyncio
 from model import AuthoringModel, ChatModel, parse_pdf
 from rouge_score import rouge_scorer
 import re
-
-# Initialize clients and scorers
-llm_client = OpenAI()
-
-hallucination_scorer = HallucinationFreeScorer(
-    model_id="openai/gpt-4o",
-    column_map={"context": "input", "output": "summary"}
-)
-
-summarization_scorer = SummarizationScorer(
-    model_id="openai/gpt-4o",
-)
+import pandas as pd
+from dotenv import load_dotenv
+import os
 
 @weave.op()
 def evaluate_rouge(summary: str, output: dict) -> dict:
@@ -30,12 +21,19 @@ def evaluate_rouge(summary: str, output: dict) -> dict:
 
 def main(config: dict):
 
-    weave.init('smle-demo/summarization-app')
+    hallucination_scorer = HallucinationFreeScorer(
+        model_id="openai/gpt-4o",
+        column_map={"context": "input", "output": "summary"}
+    )
 
-    evaluation_dataset_v1 = weave.ref('evaluation_dataset:v1').get()
-    # data = evaluation_dataset_v1.to_pandas()
-    # data = weave.Dataset.from_pandas(df)
+    summarization_scorer = SummarizationScorer(
+        model_id="openai/gpt-4o",
+    )
 
+    weave.init(f'{os.getenv("WANDB_ENTITY")}/summarization-app')
+
+    df = pd.read_json('dataset.json')
+    dataset = weave.Dataset.from_pandas(df)
 
     list_of_models = [
         "gpt-4o-mini",
@@ -43,7 +41,7 @@ def main(config: dict):
         'claude-3-5-sonnet-20240620',
         "gpt-3.5-turbo",
         "o1-preview",
-        'ft:gpt-4o-2024-08-06:weights-biases::AjD60AbG', # id for finetuned model
+        # 'ft:gpt-4o-2024-08-06:weights-biases::AjD60AbG', # id for a finetuned model in openai
     ]
 
     for model in list_of_models:
@@ -60,7 +58,7 @@ def main(config: dict):
         )
         
         evaluation = weave.Evaluation(
-            dataset=evaluation_dataset_v1,
+            dataset=dataset,
             scorers=[hallucination_scorer,
                      summarization_scorer,
                      evaluate_rouge],
@@ -70,22 +68,26 @@ def main(config: dict):
         asyncio.run(evaluation.evaluate(authoring_model, __weave={"display_name": f"{model}_evaluation"}))
 
 if __name__ == "__main__":
+    load_dotenv(".env")
 
-#     config = {
-#         'summarization_system_prompt': (
-# """You are a clinical documentation specialist tasked with authoring a detailed and structured clinical report. Use clear and precise medical language, following HL7. Ensure the report is comprehensive yet concise, maintaining a professional and factual tone.
-# Ensure the report adheres to clinical best practices and is structured logically for easy review by healthcare professionals.
-# **Clinical Report**
+    os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+    os.environ["ANTHROPIC_API_KEY"] = os.getenv("ANTHROPIC_API_KEY")
+    os.environ["WANDB_API_KEY"] = os.getenv("WANDB_API_KEY")
+    os.environ["WANDB_ENTITY"] = os.getenv("WANDB_ENTITY")
 
-# **Patient Name:** [Patient's Name]  
-# **Age:** [Patient's Age]  
-# **Diagnosis:** [Primary Diagnosis]  
+    config = {
+        'summarization_system_prompt': (
+"""You are a clinical documentation specialist tasked with authoring a detailed and structured clinical report. Use clear and precise medical language, following HL7. Ensure the report is comprehensive yet concise, maintaining a professional and factual tone.
+Ensure the report adheres to clinical best practices and is structured logically for easy review by healthcare professionals.
+**Clinical Report**
 
-# **Summary:**  
-# [Brief but structured summary of the patient's condition, treatment, and follow-up plan. Maintain a professional tone and ensure clarity for medical professionals.]
-# """        )
-#     }
+**Patient Name:** [Patient's Name]  
+**Age:** [Patient's Age]  
+**Diagnosis:** [Primary Diagnosis]  
 
-    config = {'summarization_system_prompt': 
-              'You are a helpful assistant that summarizes text clearly and concisely. Focus on capturing the key points, main arguments, and essential context. Avoid repetition or unnecessary details. When summarizing technical, academic, or domain-specific content, preserve important terminology and intent. Use a tone appropriate to the subject matter (e.g., professional for business reports, neutral for news, accessible for general content).'}
+**Summary:**  
+[Brief but structured summary of the patient's condition, treatment, and follow-up plan. Maintain a professional tone and ensure clarity for medical professionals.]
+"""        )
+    }
+
     main(config)
